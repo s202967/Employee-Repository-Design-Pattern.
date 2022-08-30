d773d8fb98227737a7ddcc784bf24d2f4cc2d68c
sunil thapa <sunil.thapa130218@gmail.com>
2022-08-30 14:42:50 +0545

    first commit

---
+++ EmployeesWeb/Repository/TaskContext.cs
@@ -0,0 +1,48 @@
+﻿using EmployeesWeb.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EmployeesWeb.Models 
+{
+    public partial class TaskContext : DbContext
+    {
+        public TaskContext(DbContextOptions<TaskContext> options)
+            : base(options)
+        {
+
+        }
+
+        public DbSet<Employee>? Employees { get; set; } = null;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Employee>(entity =>
+            {
+                entity.ToTable("Employee");
+
+                entity.Property(e => e.EmployeeId).ValueGeneratedNever();
+
+                entity.Property(e => e.Name)
+                .HasMaxLength(50)
+                .IsUnicode(false);
+
+                entity.Property(e => e.Position)
+                .HasMaxLength(100)
+                .IsUnicode(false);
+
+                entity.Property(e => e.Email)
+                .HasMaxLength(100)
+                .IsUnicode(false);
+            });
+
+            OnModelCreating(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+    
+}
---
+++ EmployeesWeb/Services/EmployeeService.cs
@@ -0,0 +1,77 @@
+﻿using EmployeesWeb.Data;
+using EmployeesWeb.Models;
+using EmployeesWeb.Repository;
+
+namespace EmployeesWeb.Services
+{
+    public class EmployeeService : IEmployeeService
+    {
+        private readonly IEmployeeRepository _employeeRepository;
+
+        public EmployeeService(IEmployeeRepository employeeRepository)
+        {
+            _employeeRepository = employeeRepository;
+        }
+
+
+        public async Task<Employee> FindByIdAsync(int id)
+        {
+            return await _employeeRepository.GetByIdAsync(id).ConfigureAwait(false);
+        }
+
+
+        public async Task<List<Employee>> GetAllAsync()
+        {
+            var resp = await _employeeRepository.GetAllAsync();
+            return resp.ToList();
+
+        }
+
+        public async Task<bool> CreateAsync(Employee employee)
+
+        {
+            await _employeeRepository.AddAsync(employee);
+            _employeeRepository.SaveChanges();
+            return true;
+        }
+
+        public async Task<bool> EditAsync(Employee employee)
+        {
+            _employeeRepository.Update(employee);
+            _employeeRepository.SaveChanges();
+            return true;
+        }
+
+        public async Task<bool> DeleteAsync(int id)
+        {
+            var employee = await FindByIdAsync(id).ConfigureAwait(false);
+            _employeeRepository.Remove(employee);
+            _employeeRepository.SaveChanges(); 
+            return true;
+        }
+
+        /*
+
+                    public async Task<bool> EditAsync(Employee employee)
+                    {
+                        _employeeRepository.Update(employee);
+                        return true;
+
+                    }
+
+                    public async Task<Employee> DetailsAsync(int? id)
+                    {
+                        return await _employeeRepository.GetByIdAsync(id.Value).ConfigureAwait(false);
+
+                    }
+
+                    public async Task<bool> DeleteAsync(int id)
+                    {
+                        var employee = await FindByIdAsync(id).ConfigureAwait(false);
+                        _employeeRepository.Remove(employee);
+                        return true;
+                    }*/
+    }
+
+
+}
